1575baaa13707fd68f77d7362f05e2939462f502
ZTEwangyan <873451621@qq.com>
2017-07-23 09:59:42 +0800

    fetch in windows

--- fetch_refactor.py
+++ fetch_refactor.py
@@ -4,39 +4,48 @@ import re
 import subprocess
 import os
 import platform
-
 addr_rep = ['https://github.com/ZTEwangyan/rep1.git','https://github.com/ZTEwangyan/rep2.git','https://github.com/ZTEwangyan/rep3.git']
-dest_rep = ['/home/wangyan/UTAC/rep1','/home/wangyan/LTE/rep2','/home/wangyan/LTE/rep3']
+dest_rep = ['E:\UTAC\\rep1','E:\LTE\\rep2',"E:\LTE\\rep3"]
 rep_dict = {}
+SEPA_PATT = re.compile(r'\\')
 FORMAT = '--pretty=format: %h %an %ad  %s'
 # create a dict to store the reposition and the branches
-def last_of_path(repo_str):
-    pattern = re.compile(r'/')
-    res      = re.split(pattern,repo_str)
+def init():
+    plat = platform.platform()
+    if 'Linux' in plat:
+        SEPA_PATT = re.compile(r'/')
+def last_of_path(patt,repo_str):
+    #pattern = re.compile(r'\\')
+    res      = re.split(patt,repo_str)
     return res[len(res)-1]
+'''def last_of_path2(repo_str):
+    pattern = re.compile(r'/')
+    res     = re.split(pattern,repo_str)
+    return res[len(res)-1]'''
 def fetch_all_branches(repo,index):
     # get the master branch:
     branches = []
     cmd_master = ['git', 'clone', addr_rep[index], dest_rep[index]]
-    subprocess.check_output(cmd_master)
+    subprocess.check_output(cmd_master,shell=True)
     # get the other branches:
+    # print('subprocess exec sucessfully')
     os.chdir(dest_rep[index])
     cmd_slave = ['git', 'branch', '-a']
-    stdout = subprocess.check_output(cmd_slave)
+    stdout = subprocess.check_output(cmd_slave,shell=True)
     for each in stdout.splitlines():
         if re.match('\*.',each,re.IGNORECASE):
             master = each[2:]
         elif (not re.match('.*master', each, re.IGNORECASE)):
-            branches.append(last_of_path(each))
+            branches.append(last_of_path(re.compile(r'/'),each))
     for branch in branches:
         command_new_branch = ['git','checkout','-b',branch,'origin/'+branch]
-        subprocess.check_output(command_new_branch)
+        subprocess.check_output(command_new_branch,shell=True)
     branches.append(master)
     # save the branches in repository
     rep_dict.update({repo:branches})
 def fetch():
     for index in range(0,len(dest_rep)):
-        repo_name = last_of_path(dest_rep[index])
+        repo_name = last_of_path(SEPA_PATT,dest_rep[index])
         fetch_all_branches(repo_name,index)
 def getlog():
     while(True):
@@ -52,10 +61,11 @@ def getlog():
             branch = raw_input('which branch\'s log do you want?\n')
             if branch in rep_dict.get(rep_in):
                 check_cmd = ['git','checkout',branch]
-                subprocess.check_output(check_cmd)
+                subprocess.check_output(check_cmd,shell=True)
                 cmd = ['git','log',FORMAT]
-                stdout = subprocess.check_output(cmd)
+                stdout = subprocess.check_output(cmd,shell=True)
                 print(stdout)
 if __name__ == '__main__':
+    init()
     fetch()
     getlog()
